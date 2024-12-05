0a78e1075337e5100f178bd06c5202560a6ccb38
SergioSuaSan <148342257+SergioSuaSan@users.noreply.github.com>
2024-12-05 14:08:19 +0100

    5/12

--- examenv1hilos/src/psp_2024/hilos/bloqueos/con_mas_de_2hilos/AlternateCharPrinter.java
+++ examenv1hilos/src/psp_2024/hilos/bloqueos/con_mas_de_2hilos/AlternateCharPrinter.java
@@ -1,4 +1,4 @@
-package solucionProfesor;
+package psp_2024.hilos.bloqueos.con_mas_de_2hilos;
 public class AlternateCharPrinter {
 
     public static char ch = 65;
@@ -20,7 +20,7 @@ class AlternateCharRunner implements Runnable {
 
     private char ch;
     private Object lock;
-    private static int runnerCount;
+	private  static  int runnerCount;
 
     public AlternateCharRunner(char ch, Object lock) {
         this.ch = ch;
@@ -31,34 +31,52 @@ class AlternateCharRunner implements Runnable {
     @Override
     public void run() {
     	
-    	
+    	// System.out.println(runnerCount);
     	
         while (true) {
             synchronized (lock) {
             	
-            	
+				
+				  System.out.println(); System.out.println("Hay " + Thread.activeCount() +
+				  " hilos activos"); System.out.println();
+				 
             	try {
-					Thread.sleep(30);
+					Thread.sleep(100);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				} 
+				}
+				/*
+				 * System.out.println(); System.out.println("Hay " + Thread.activeCount() +
+				 * " hilos activos"); System.out.println();
+				 */
+            	
             	
             	
                 while (ch != AlternateCharPrinter.ch) {
                     try {
                     	
-                   	Thread.sleep(100); 
+                   	Thread.sleep(100); /*
+                        System.out.println();
+                        System.out.println("Hay " + Thread.activeCount() + " hilos activos");
+                        System.out.println();
+                        */
                     	
                         lock.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-
+                    System.out.println(runnerCount);
+                    
+                    /*	Thread.sleep(1000);
+                    System.out.println();
+                    System.out.println("Hay " + Thread.activeCount() + " hilos activos");
+                    System.out.println();
+                    */
                     
                 }
                System.out.println(AlternateCharPrinter.ch++);
-
+               //System.out.println(runnerCount);
                 if (AlternateCharPrinter.ch == (65 + runnerCount)) {
                     AlternateCharPrinter.ch = 65;
                 }
